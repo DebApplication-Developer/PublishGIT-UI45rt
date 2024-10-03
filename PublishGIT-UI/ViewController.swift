6ceec945101875ad22913da54ebc9b91eec9c8fa
coderpath <palashiosdeveloper@gmail.com>
2024-10-03 08:04:40 +0530

    Initial Commit

---
+++ PublishGIT-UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PublishGIT-UI
+//
+//  Created by CoderPath on 03/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var showText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showText.text = "asm12356778444nmnm"
+    }
+
+
+}
+
